672daa16cd4015fe590728ff19b494d38c0db448
laststnd <786alijafri786@gmail.com>
2016-08-13 15:20:04 +0530

    Optimized

--- Torrentz.user.js
+++ Torrentz.user.js
@@ -1,61 +1,72 @@
 // ==UserScript==
-// @name        Torrentz
+// @name        Torrentz2
 // @namespace   Laststnd
 // @description Magnetic link on torrentz.in
-// @include     *torrentz.*
 // @include     *torrentz2.*
 // @version     1.1
 // @grant       none
 // ==/UserScript==
 
-
+// sets the hash value
 var urls="magnet:?xt=urn:btih:";
-var d=document.getElementsByTagName('div');
-for(i=0;i<d.length;i++)
-{
-    if(d[i].innerHTML.indexOf('info_hash:')>=0 && d[i].innerHTML.indexOf('div')<0)
-    {
-        str=d[i].innerHTML;
-        str=str.substring(str.indexOf(':')+2);
-        urls=urls+str+"&dn=";
-        break;
-    }
-}
+str=document.getElementsByClassName('trackers')[0].getElementsByTagName('h2')[0].childNodes[1].nodeValue;
+str=str.substring(str.indexOf('hash ')+5);
+urls=urls+str+"&dn=";
+// console.log(urls);
 
-var n=document.getElementsByTagName('h2');
-for(i=0;i<n.length;i++)
-{
-    if(n[i].innerHTML.indexOf('Related to')>=0)
-    {
-        str2="";
-        str=n[i].getElementsByTagName('a')[0].innerHTML;
-        for(j=0;j<str.length;j++)
-        {
-            if(str.charAt(j)==' ')
-                str2=str2+'+';
-            else
-                str2=str2+str.charAt(j);
-        }
-        urls=urls+str2;
-        break;
-    }
-}
-var a=document.getElementsByTagName('a');
+// sets the file name
+var fileName=document.getElementsByClassName('download')[0].getElementsByTagName('h2')[0].childNodes[0].innerText;
+// console.log(fileName);
+fileName = fileName.replace(/ /g,'+');
+urls=urls+fileName;
+
+console.log(urls);
+
+// sets the trackers
+var tr=document.getElementsByClassName('trackers')[0].getElementsByTagName('dl');
 pos=0;
 txtFlag=0;
-for(i=0;i<a.length;i++)
+for(i=0;i<tr.length;i++)
 {
-    if(a[i].href.indexOf('tracker_')>=0)
-        urls=urls+"&tr="+a[i].innerHTML;
-    else if(a[i].rel.indexOf('e')>=0 && txtFlag==0)
-    {
-        pos=i;
-        txtFlag=1;
-    }
+    urls = urls +"&tr="+ tr[i].childNodes[0].innerText;
 }
-//alert(urls);
-a[pos].href=urls;
-a[pos].innerHTML=a[pos].innerHTML.substring(0,a[pos].innerHTML.lastIndexOf('</span>')+7);
-a[pos].getElementsByTagName('span')[0].style="background: transparent url('http://i57.tinypic.com/aoxdol.jpg') no-repeat 5px center;color : black;";
-a[pos].getElementsByTagName('span')[1].innerHTML="<Font color='red'>Magnetic Link --- By L@$t$tnD</font>";
-a[pos].getElementsByTagName('span')[0].innerHTML="<font color='black'>Magnetic Link</font>";
+
+console.log(urls);
+
+var newDL = document.createElement('dl');
+var newDT = document.createElement('dt');
+var newLink = document.createElement('a');
+var newSpanLogo = document.createElement('span');
+var newSpanLink = document.createElement('span');
+var newSpanDesc = document.createElement('span');
+
+newLink.href = urls;
+newDT.appendChild(newLink);
+
+newSpanLogo.style = "background: transparent url('http://i57.tinypic.com/aoxdol.jpg') no-repeat 5px center;color : black;";
+newSpanLogo.setAttribute("class", "insites sprite");
+
+newSpanLink.innerText = "Magnetic Link";
+newSpanLink.setAttribute("class","u");
+
+newSpanDesc.style = "color:red";
+newSpanDesc.innerText = "Magnetic Link --- By L@$t$tnD";
+newSpanDesc.setAttribute("class", "n");
+
+newLink.appendChild(newSpanLogo);
+newLink.appendChild(newSpanLink);
+newLink.appendChild(newSpanDesc);
+
+newDL.appendChild(newDT);
+var list = document.getElementsByClassName('download')[0].childNodes;
+var i;
+// get the tagName of position of 1st link in downloading sites
+for(i=0;i<list.length && list[i].tagName!="DL";i++);
+
+document.getElementsByClassName('download')[0].insertBefore(newDL,list[i]); //  to insert the magnetic link at top of existing links
+
+// a[pos].href=urls;
+// a[pos].innerHTML=a[pos].innerHTML.substring(0,a[pos].innerHTML.lastIndexOf('</span>')+7);
+// a[pos].getElementsByTagName('span')[0].style="background: transparent url('http://i57.tinypic.com/aoxdol.jpg') no-repeat 5px center;color : black;";
+// a[pos].getElementsByTagName('span')[1].innerHTML="<Font color='red'>Magnetic Link --- By L@$t$tnD</font>";
+// a[pos].getElementsByTagName('span')[0].innerHTML="<font color='black'>Magnetic Link</font>";
